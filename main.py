a269a2e9f09f9a0fe6016dcb96e7fe16abf64730
LALOL <89741263+Its-LALOL@users.noreply.github.com>
2022-09-03 14:16:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -23,15 +23,14 @@ LALOL=Fore.RED +"""
 ███████╗██║░░██║███████╗╚█████╔╝███████╗
 ╚══════╝╚═╝░░╚═╝╚══════╝░╚════╝░╚══════╝
 """
-os.system(f'cls && title Selfbot By LALOL' if os.name == 'nt' else 'clear')
+os.system(f'cls && title Selfbot by LALOL' if os.name == 'nt' else 'clear')
 print(LALOL)
-print(Fore.GREEN + "Selfbot By LALOL\n" + Fore.RED)
-
+print(Fore.GREEN + "Selfbot by LALOL\n" + Fore.RED)
 
 pref=config['Prefix']
-
 bot = commands.Bot(command_prefix=pref, case_insensitive=True, self_bot=True)
 version=0.1
+bot.remove_command('help')
 
 @bot.event
 async def on_ready():
@@ -49,9 +48,21 @@ async def on_command_error(ctx, error):
 		print(Fore.RED + f"[ERROR] Неправильный аргумент!")
 	else:
 		print(Fore.RED + f"[ERROR] {error}")
-@bot.command(name='bot')
+@bot.command(aliases=['хелп', 'помощь'])
+async def help(ctx, cat=None):
+	if cat==None:
+		await ctx.message.edit(content=f'**__Selfbot by LALOL__\n\n:screwdriver:`{pref}help Tools` - Полезные команды\n:information_source:`{pref}help Info` - Команды для получения информации\n:joy:`{pref}help Fun` - Развлекательные команды\n\n:robot:`{pref}bot` - Получение ссылки на установку селф бота**')
+		return
+	cat=cat.lower()
+	if cat=='tools':
+		await ctx.message.edit(content=f'**__Selfbot by LALOL__\n\n`{pref}status [Тип статуса] [Текст]` - Меняет статус**')
+	if cat=='info':
+		await ctx.message.edit(content=f'**__Selfbot by LALOL__\n\n`{pref}server` - Информация о сервере\n`{pref}user [ID/Пинг]` - Информация об аккаунте**')
+	if cat=='fun':
+		await ctx.message.edit(content=f'**__Selfbot by LALOL__\n\n`{pref}troll [ID/Пинг]` - Удаление всех сообщений пользователя\n`{pref}untroll` - Выключение команды troll**')
+@bot.command(name='bot', aliases=['selfbot', 'бот', 'селфбот'])
 async def __bot(ctx):
-	await ctx.message.edit(content='**Selfbot By LALOL\n\nСсылка: https://github.com/Its-LALOL/Discord-Selfbot **')
+	await ctx.message.edit(content='**__Selfbot by LALOL__\n\nСсылка: https://github.com/Its-LALOL/Discord-Selfbot**')
 for filename in os.listdir("./cogs"):
 	if filename.endswith(".py"):
 		bot.load_extension(f"cogs.{filename[:-3]}")
