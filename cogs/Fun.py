32e22908a21828d97c1f20bd65588ef5145cd8bc
LALOL <89741263+Its-LALOL@users.noreply.github.com>
2022-09-09 21:43:10 +0300

    Update Fun.py

--- cogs/Fun.py
+++ cogs/Fun.py
@@ -21,9 +21,18 @@ class Fun(commands.Cog):
 		troll['user_id']=0
 	@commands.Cog.listener()
 	async def on_message(self, message):
-		try:message.guild
+		try:
+			if message.author.id==troll['user_id'] and message.guild.id==troll['server_id']:
+				await message.delete()
 		except:return
-		if message.author.id==troll['user_id'] and message.guild.id==troll['server_id']:
-			await message.delete()
+	@commands.command(aliases=['react', 'reactions', 'реакция', 'реакции', 'reactionall'])
+	async def reaction(self, ctx, emoji, amount: int=15):
+		await ctx.message.delete()
+		messages=await ctx.channel.history(limit=amount).flatten()
+		reactioned=0
+		for message in messages:
+			await message.add_reaction(emoji)
+			reactioned+=1
+		await ctx.send(f"**__Selfbot by LALOL__\n\n:white_check_mark: Успешно поставил {reactioned} реакций!**")
 def setup(bot):
 	bot.add_cog(Fun(bot))
