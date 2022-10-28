52f659e894e9563c468b82cdba9f64c6a56020a6
LALOL <89741263+Its-LALOL@users.noreply.github.com>
2022-10-28 21:08:59 +0300

    Update Moderation.py

--- cogs/Moderation.py
+++ cogs/Moderation.py
@@ -13,21 +13,21 @@ class Moderation(commands.Cog):
 		except:
 			await ctx.message.edit(content=f'**__Selfbot by LALOL__\n\n:x: Не удалось забанить пользователя `{user}`**')
 			return
-		await ctx.message.edit(content=f'**__Selfbot by LALOL__\n\n:white_check_mark: Пользователь `{user}` успешно был забанен по причине `{reason}`**')
+		await ctx.message.edit(content=f'**__Selfbot by LALOL__\n\n:dagger: Пользователь `{user}` успешно был забанен по причине `{reason}`**')
 	@commands.command(aliases=['разбанить', 'разбан'])
 	async def unban(self, ctx, user:discord.User):
 		try: await ctx.guild.unban(user)
 		except:
 			await ctx.message.edit(content=f'**__Selfbot by LALOL__\n\n:x: Не удалось разбанить пользователя `{user}`**')
 			return
-		await ctx.message.edit(content=f'**__Selfbot by LALOL__\n\n:white_check_mark: Пользователь `{user}` успешно был разбанен**')
+		await ctx.message.edit(content=f'**__Selfbot by LALOL__\n\n:ok_hand: Пользователь `{user}` успешно был разбанен**')
 	@commands.command(aliases=['кик', 'выгнать', 'кикнуть'])
 	async def kick(self, ctx, user:discord.Member, *, reason='Причина не указана'):
 		try: await ctx.guild.kick(user, reason=reason)
 		except:
 			await ctx.message.edit(content=f'**__Selfbot by LALOL__\n\n:x: Не удалось кикнуть пользователя `{user}`**')
 			return
-		await ctx.message.edit(content=f'**__Selfbot by LALOL__\n\n:white_check_mark: Пользователь `{user}` успешно был кикнут по причине `{reason}`**')
+		await ctx.message.edit(content=f'**__Selfbot by LALOL__\n\n:door: Пользователь `{user}` успешно был кикнут по причине `{reason}`**')
 	@commands.command(aliases=['мут', 'мьют', 'замутить', 'замьютить'])
 	async def mute(self, ctx, user:discord.Member, time, *, reason='Причина не указана'):
 		if time.endswith('s') or time.endswith('с'):
@@ -45,14 +45,14 @@ class Moderation(commands.Cog):
 		if response==403:
 			await ctx.message.edit(content=f'**__Selfbot by LALOL__\n\n:x: Не удалось замутить пользователя `{user}`**')
 			return
-		await ctx.message.edit(content=f'**__Selfbot by LALOL__\n\n:white_check_mark: Пользователь `{user}` был успешно замучен по причине `{reason}` на `{time}`**')
+		await ctx.message.edit(content=f'**__Selfbot by LALOL__\n\n:mute: Пользователь `{user}` был успешно замучен по причине `{reason}` на `{time}`**')
 	@commands.command(aliases=['размут', 'размьют', 'размутить', 'размьютить'])
 	async def unmute(self, ctx, user:discord.Member, *, reason='Причина не указана'):
 		response=requests.patch(f'https://discord.com/api/v9/guilds/{ctx.guild.id}/members/{user.id}', headers={"Authorization": self.bot.http.token, 'X-Audit-Log-Reason': quote(reason)}, json={"communication_disabled_until": 0})
 		if response==403:
 			await ctx.message.edit(content=f'**__Selfbot by LALOL__\n\n:x: Не удалось размутить пользователя `{user}`**')
 			return
-		await ctx.message.edit(content=f'**__Selfbot by LALOL__\n\n:white_check_mark: Пользователь `{user}` был успешно размучен по причине `{reason}`**')
+		await ctx.message.edit(content=f'**__Selfbot by LALOL__\n\n:sound: Пользователь `{user}` был успешно размучен по причине `{reason}`**')
 	@commands.command(alises=['слоумод'])
 	async def slowmode(self, ctx, time):
 		if time.endswith('s') or time.endswith('с'):
@@ -68,7 +68,7 @@ class Moderation(commands.Cog):
 			await ctx.message.edit(content=f'**__Selfbot by LALOL__\n\n:x: Слоумод не может быть больше 6 часов!**')
 			return
 		await ctx.channel.edit(slowmode_delay=seconds)
-		await ctx.message.edit(content=f'**__Selfbot by LALOL__\n\n:white_check_mark: Слоумод канала был успешно изменён на {seconds} секунд**')
+		await ctx.message.edit(content=f'**__Selfbot by LALOL__\n\n:timer: Слоумод канала был успешно изменён на {seconds} секунд**')
 	@commands.command(alises=['channelnuke', 'nuke_channel', 'channel_nuke'])
 	async def nukechannel(self, ctx):
 		new_channel=await ctx.channel.clone()
