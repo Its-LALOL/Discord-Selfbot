32a3753f22cbb279a6b1e7dfcf89cabfa26a2715
Puro <103608145+PuroSlavKing@users.noreply.github.com>
2023-07-10 20:57:05 +0300

    Update Info.py

--- cogs/Info.py
+++ cogs/Info.py
@@ -25,7 +25,7 @@ class Info(commands.Cog):
 		if ctx.guild.owner is None:
 			owner='`Unknown`'
 		createdat=round(ctx.guild.created_at.timestamp())
-		await ctx.message.edit(content=f'__**Selfbot**__\n\n```Базовое```**Имя: `{ctx.guild.name}`\nID: `{ctx.guild.id}`\nСоздатель: {owner}\nСоздан: <t:{createdat}> (<t:{createdat}:R>)```Участники и боты [Информация может быть не точная]```Участников: `{users}`\nБотов: `{bots}`\nВсего: `{users+bots}` ```Каналы```Текстовых: `{len(ctx.guild.text_channels)}`\nГолосовых: `{len(ctx.guild.voice_channels)}`\nКатегорий: `{len(ctx.guild.categories)}`\nВсего: `{len(ctx.guild.channels)}` ```Роли```Пингующихся: `{mentions}`\nАдминских: `{admins}`\nВсего: `{len(ctx.guild.roles)}`**')
+		await ctx.message.edit(content=f'```Базовое```**Имя: `{ctx.guild.name}`\nID: `{ctx.guild.id}`\nСоздатель: {owner}\nСоздан: <t:{createdat}> (<t:{createdat}:R>)```Участники и боты [Информация может быть не точная]```Участников: `{users}`\nБотов: `{bots}`\nВсего: `{users+bots}` ```Каналы```Текстовых: `{len(ctx.guild.text_channels)}`\nГолосовых: `{len(ctx.guild.voice_channels)}`\nКатегорий: `{len(ctx.guild.categories)}`\nВсего: `{len(ctx.guild.channels)}` ```Роли```Пингующихся: `{mentions}`\nАдминских: `{admins}`\nВсего: `{len(ctx.guild.roles)}`**')
 	@commands.command(aliases=['юзер', 'участник', 'member', 'инфо', 'информация', 'info', 'information'])
 	async def user(self, ctx, user:discord.User=None):
 		if user is None:
@@ -37,7 +37,7 @@ class Info(commands.Cog):
 			if user.bot:
 				bot='Да'
 			createdat=round(user.created_at.timestamp())
-			await ctx.message.edit(content=f'**__Selfbot__\n\nИмя: `{user.name}`\nТег: `{user.discriminator}`\nID: `{user.id}`\nБот: `{bot}`\nАккаунт создан: <t:{createdat}> (<t:{createdat}:R>)**')
+			await ctx.message.edit(content=f'**Имя: `{user.name}`\nТег: `{user.discriminator}`\nID: `{user.id}`\nБот: `{bot}`\nАккаунт создан: <t:{createdat}> (<t:{createdat}:R>)**')
 		else:
 			user=user1
 			owner='Нет'
@@ -67,7 +67,7 @@ class Info(commands.Cog):
 			admin='Нет'
 			if user.guild_permissions.administrator:
 				admin='Да'
-			await ctx.message.edit(content=f'**__Selfbot__\n\nИмя: `{user.name}`\nТег: `{user.discriminator}`\nID: `{user.id}`\n{nick}Бот: `{bot}`\nСоздатель: `{owner}`\nАдмин: `{admin}`\nСамая высокая роль: `@{user.top_role.name}`\n{voice}Статус: `{status}`\nАккаунт создан: <t:{createdat}> (<t:{createdat}:R>)\nЗашёл на сервер: <t:{joinedat}> (<t:{joinedat}:R>)**')
+			await ctx.message.edit(content=f'**Имя: `{user.name}`\nТег: `{user.discriminator}`\nID: `{user.id}`\n{nick}Бот: `{bot}`\nСоздатель: `{owner}`\nАдмин: `{admin}`\nСамая высокая роль: `@{user.top_role.name}`\n{voice}Статус: `{status}`\nАккаунт создан: <t:{createdat}> (<t:{createdat}:R>)\nЗашёл на сервер: <t:{joinedat}> (<t:{joinedat}:R>)**')
 	@commands.command(aliases=['токен'])
 	async def token(self, ctx, token):
 		headers={'authorization': token}
@@ -81,12 +81,12 @@ class Info(commands.Cog):
 			friends=len(r2.json())
 			dms=len(r1.json())
 			guilds=len(r3.json())
-			await ctx.message.edit(content=f"**__Selfbot__\n\nТокен рабочий :white_check_mark:\nАккаунт: `{info['username']}#{info['discriminator']}`**\n**ID: `{info['id']}`**\n**Почта: `{info['email']}`**\n**Телефон: `{info['phone']}`**\n**Страна: :flag_{info['locale']}:**\n**Открытых лс: `{dms}`**\n**Друзей: `{friends}`**\n**Серверов: `{guilds}`**")
+			await ctx.message.edit(content=f"**Токен рабочий :white_check_mark:\nАккаунт: `{info['username']}#{info['discriminator']}`**\n**ID: `{info['id']}`**\n**Почта: `{info['email']}`**\n**Телефон: `{info['phone']}`**\n**Страна: :flag_{info['locale']}:**\n**Открытых лс: `{dms}`**\n**Друзей: `{friends}`**\n**Серверов: `{guilds}`**")
 		elif token_check.status_code == 401:
-			await ctx.message.edit(content=f"**__Selfbot__\n\nТокен** `{token}`**\nНе рабочий! :x:**")
+			await ctx.message.edit(content=f"**Токен** `{token}`**\nНе рабочий! :x:**")
 		elif token_check.status_code == 403:
 			response=requests.get('https://discord.com/api/users/@me',headers=headers)
 			info=response.json()
-			await ctx.message.edit(content=f"**__Selfbot by LALOL__\n\nТокен рабочий, но требует привязку почты/телефона :warning:\nАккаунт: `{info['username']}#{info['discriminator']}`**\n**ID: `{info['id']}`**\n**Почта: `{info['email']}`**\n**Телефон: `{info['phone']}`**\n**Страна: `{info['locale']}`**")
+			await ctx.message.edit(content=f"**Токен рабочий, но требует привязку почты/телефона :warning:\nАккаунт: `{info['username']}#{info['discriminator']}`**\n**ID: `{info['id']}`**\n**Почта: `{info['email']}`**\n**Телефон: `{info['phone']}`**\n**Страна: `{info['locale']}`**")
 def setup(bot):
 	bot.add_cog(Info(bot))
